6c8104feaccced9db1c8293d4c358772a17d831f
Koray K <3178435+siniradam@users.noreply.github.com>
2021-05-02 21:08:56 -0700

    Environment file check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-require("dotenv").config();
 const colors = require("colors");
+const env = require("dotenv").config();
+if (env.error) {
+  throw "Environment file is required.".red;
+}
+
 const connectDB = require("./utils/db");
 connectDB();
 const app = require("./app");
