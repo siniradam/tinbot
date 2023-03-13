44883e204f521a651cd8221b45c48c1409a598c8
Koray K <3178435+siniradam@users.noreply.github.com>
2023-03-12 17:34:19 -0700

    path update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const colors = require("colors");
-const env = require("dotenv").config();
+const path = require("path");
+const env = require("dotenv").config({ path: path.join(__dirname, ".env") });
 if (env.error) {
   throw "No environment file is found.".red;
 }
